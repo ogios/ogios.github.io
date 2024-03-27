85ccd73cac6707a9b4c31fda321f8e082087728b
ogios <2134692955@qq.com>
2024-03-27 18:37:52 +0800

    feat: perserve certian files; bump blogs

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,44 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import Unocss from 'unocss/vite'
 import fs from 'fs-extra'
 import path from 'path'
-function rmStuff(pattern: string, cwd = './dist/') {
+function rmStuff(patterns: string[], cwd: string) {
+  const l: string[] = (() => {
+    const ls: string[] = []
+    const pps = patterns.map((v) => globFiles(v, cwd))
+    pps.forEach((v) => ls.push(...v))
+    return ls
+  })()
+  l.forEach((v) => {
+    fs.rm(path.join(cwd, v))
+  })
+}
+
+function globFiles(pattern: string, cwd?: string) {
   const a = new Bun.Glob(pattern)
   const res = a.scanSync({ cwd, dot: true })
+  const l: string[] = []
   let r = res.next()
   while (!r.done) {
     if (r.value) {
-      console.log('found:', r.value)
-      fs.rm(path.join(cwd, r.value))
+      l.push(r.value)
     }
     r = res.next()
   }
+  return l
+}
+
+function onlyPreserve(patterns: string[], cwd: string) {
+  const all = fs.readdirSync(cwd)
+  const preserve: string[] = (() => {
+    const ls: string[] = []
+    const pps = patterns.map((v) => globFiles(v, cwd))
+    pps.forEach((v) => ls.push(...v))
+    return ls
+  })()
+  const remove = all.map((v) => (preserve.indexOf(v) === -1 ? v : undefined)).filter((v) => v)
+  remove.forEach((v) => {
+    fs.removeSync(path.join(cwd, v))
+  })
 }
 
 // https://vitejs.dev/config/
@@ -26,9 +53,8 @@ export default defineConfig({
     {
       name: 'readme',
       writeBundle() {
-        rmStuff('**/README.md')
-        rmStuff('**/.git')
-        rmStuff('blogs/md_metadata_gen.ts')
+        onlyPreserve(['*.md', 'meta.json'], './dist/blogs/')
+        rmStuff(['README.md'], './dist/blogs/')
       },
     },
   ],
